794cb4743aff75b38405d9362ded00d9ea01c81e
Roman Son <son.romario@gmail.com>
2024-04-11 17:37:09 +0300

    Clear data in product detail page

--- pages/product/sidebar/[id].vue
+++ pages/product/sidebar/[id].vue
@@ -1,13 +1,13 @@
 <template>
-<Header/>
+  <Header/>
   <div>
-    <WidgetsBreadcrumbs :title="getDetail.title" />
+    <WidgetsBreadcrumbs :title="getDetail.title"/>
     <section class="section-b-space">
       <div class="collection-wrapper">
         <div class="container">
           <div class="row">
             <div class="col-lg-3">
-              <WidgetsProductSidebar />
+              <WidgetsProductSidebar/>
             </div>
             <div class="col-lg-9 col-sm-12 col-xs-12 productdetail">
               <div class="container-fluid">
@@ -16,16 +16,16 @@
                     <swiper @swiper="onSwiper" :slidesPerView="1" :spaceBetween="20" class="swiper-wrapper h-auto">
                       <swiper-slide class="swiper-slide" v-for="(product, index) in getDetail.images" :key="index">
                         <img :src="getImgUrl(product.src)" :id="product.image_id" class="img-fluid bg-img"
-                          :alt="product.alt" />
+                             :alt="product.alt"/>
                       </swiper-slide>
                     </swiper>
                     <div class="row">
                       <div class="col-12 slider-nav-images">
                         <swiper :slidesPerView="3" slide-active-class="true" :spaceBetween="20" class="swiper-wrapper">
                           <swiper-slide class="swiper-slide" v-for="(product, index) in getDetail.images" :key="index"
-                            :class="slideId == index ? 'product-slider-active' : ''">
+                                        :class="slideId == index ? 'product-slider-active' : ''">
                             <img :src="getImgUrl(product.src)" :id="product.image_id" class="img-fluid bg-img"
-                              alt="product.alt" @click="slideTo(index)" />
+                                 alt="product.alt" @click="slideTo(index)"/>
                           </swiper-slide>
                         </swiper>
                       </div>
@@ -55,15 +55,15 @@
                           <div class="input-group">
                             <span class="input-group-prepend">
                               <button type="button" class="btn quantity-left-minus" data-type="minus" data-field
-                                @click="decrement()">
+                                      @click="decrement()">
                                 <i class="ti-angle-left"></i>
                               </button>
                             </span>
                             <input type="text" name="quantity" class="form-control input-number"
-                              :disabled="counter > getDetail.stock" v-model="counter" />
+                                   :disabled="counter > getDetail.stock" v-model="counter"/>
                             <span class="input-group-prepend">
                               <button type="button" class="btn quantity-right-plus" data-type="plus" data-field
-                                @click="increment()">
+                                      @click="increment()">
                                 <i class="ti-angle-right"></i>
                               </button>
                             </span>
@@ -72,13 +72,15 @@
                       </div>
                       <div class="product-buttons">
                         <nuxt-link :to="{ path: '/page/account/cart' }">
-                          <button class="btn btn-solid" title="Добавить в корзину" @click="addToCart(getDetail, counter)"
-                            :disabled="counter > getDetail.stock">Добавить в корзину</button>
+                          <button class="btn btn-solid" title="Добавить в корзину"
+                                  @click="addToCart(getDetail, counter)"
+                                  :disabled="counter > getDetail.stock">Добавить в корзину
+                          </button>
                         </nuxt-link>
                       </div>
                       <div class="border-product">
                         <h6 class="product-title"> описание товара</h6>
-                        <p>{{ getDetail.description.substring(0, 200) + "...." }}</p>
+                        <p>{{ getDetail.description.substring(0, 200) + '....' }}</p>
                       </div>
                     </div>
                   </div>
@@ -88,9 +90,9 @@
           </div>
         </div>
       </div>
-      <WidgetsRelatedProducts :productTYpe="productTYpe" :productId="productId" />
+<!--      <WidgetsRelatedProducts :productTYpe="productTYpe" :productId="productId"/>-->
       <div class="modal fade " id="modal-1" aria-hidden="true" tabindex="-1" role="dialog"
-        aria-labelledby="modal-cartLabel">
+           aria-labelledby="modal-cartLabel">
         <div class="modal-dialog modal-md modal-dialog-centered">
           <div class="modal-content">
             <div class="row">
@@ -99,10 +101,9 @@
                   <span>×</span>
                 </button>
                 {{ getDetail.title }} -->
-                <div class="modal-header"><h5 class="modal-title">{{ getDetail.title }}</h5><button type="button" class="btn-close" aria-label="Close" data-bs-dismiss="modal"></button></div>
-              </div>
-              <div>
-                <img src="/images/size-chart.jpg" alt="size-chart" class="img-fluid size-chart" />
+                <div class="modal-header"><h5 class="modal-title">{{ getDetail.title }}</h5>
+                  <button type="button" class="btn-close" aria-label="Close" data-bs-dismiss="modal"></button>
+                </div>
               </div>
             </div>
           </div>
@@ -110,7 +111,7 @@
       </div>
     </section>
   </div>
-  <Footer />
+  <Footer/>
 </template>
 <script>
 
@@ -118,29 +119,22 @@
 import {
   Swiper,
   SwiperSlide
-} from "swiper/vue";
+} from 'swiper/vue';
 import 'swiper/css';
-import { mapState } from 'pinia'
-import { useProductStore } from "~~/store/products";
-import { useCartStore } from "~~/store/cart";
+import {mapState} from 'pinia'
+import {useProductStore} from '~~/store/products';
+import {useCartStore} from '~~/store/cart';
 
 export default {
   components: {
-
-     Swiper, SwiperSlide,
+    Swiper, SwiperSlide,
   },
 
   data() {
     return {
       slideId: 0,
       counter: 1,
-      activeColor: '',
-      selectedSize: '',
       qty: '',
-      size: [],
-      productTYpe: '',
-      productId: '',
-
     }
   },
   computed: {
@@ -152,14 +146,10 @@ export default {
       return useProductStore().changeCurrency
     },
     getDetail: function () {
-
       return useProductStore().getProductById(this.$route.params.id)
-
-
     },
 
   },
-
 
   methods: {
     onSwiper(swiper) {
@@ -168,38 +158,15 @@ export default {
     priceCurrency: function () {
       useProductStore().changeCurrency()
     },
-    addToWishlist: function (product) {
-      useProductStore().addToWishlist(product)
-    },
     discountedPrice(product) {
       const price = (product.price - (product.price * product.discount / 100)) * this.curr.curr
       return price
 
     },
-    // Related Products display
-    relatedProducts() {
-      this.productTYpe = this.getDetail.type
-      this.productId = this.getDetail.id
-    },
-    // Display Unique Color
-    Color(variants) {
-      const uniqColor = []
-      for (let i = 0; i < Object.keys(variants).length; i++) {
-        if (uniqColor.indexOf(variants[i].color) === -1) {
-          uniqColor.push(variants[i].color)
-        }
-      }
-      return uniqColor
-    },
     // add to cart
     addToCart: function (product, qty) {
       product.quantity = qty || 1
       useCartStore().addToCart(product)
-    },
-    buyNow: function (product, qty) {
-      product.quantity = qty || 1
-      useCartStore().addToCart(product)
-      this.$router.push('/page/account/checkout')
     },
     // Item Count
     increment() {
@@ -207,10 +174,6 @@ export default {
     },
     decrement() {
       if (this.counter > 1) this.counter--
-    },
-    // Change size variant
-    changeSizeVariant(variant) {
-      this.selectedSize = variant
     },
     getImgUrl(path) {
       return ('/images/' + path)
@@ -220,30 +183,6 @@ export default {
 
       this.slideId = id
     },
-
-
-    sizeVariant(id, slideId, color) {
-      this.swiper.slideTo(slideId)
-      this.size = []
-      this.activeColor = color
-      this.getDetail.variants.filter((item) => {
-        if (id === item.image_id) {
-          this.size.push(item.size)
-        }
-      })
-    }
-  },
-   mounted() {
-
-    // For displaying default color and size on pageload
-    this.uniqColor = this.getDetail.variants[0].color
-    this.sizeVariant(this.getDetail.variants[0].image_id)
-    // Active default color
-    this.activeColor = this.uniqColor
-    this.changeSizeVariant(this.getDetail.variants[0].size)
-    // related product type
-    this.relatedProducts()
-
   },
 }
 </script>
