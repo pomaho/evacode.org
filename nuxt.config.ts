c136d63f1c9939f0feb860c243b3ca7d5d9a07f9
Roman Son <son.romario@gmail.com>
2024-04-12 21:53:59 +0300

    Clean config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,53 +2,45 @@
 
 // @ts-ignore
 export default defineNuxtConfig({
-  $development: undefined, $env: undefined, $meta: undefined, $production: undefined, $test: undefined,
-  routeRules: {
+    $development: undefined, $env: undefined, $meta: undefined, $production: undefined, $test: undefined,
+    routeRules: {},
 
-  },
-
-  app: {
-    head: {
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
-      title: 'Evacode - интернет магазин корейской косметики',
-      meta: [
-        { name: 'description', content: 'Evacode - интернет магазин корейской косметики.' }
-      ],
-      link: [
-          { rel: 'icon', type: 'image/png', href: "/images/evacode/favicon.ico" }
-      ],
+    app: {
+        head: {
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1',
+            title: 'Evacode - интернет магазин корейской косметики',
+            meta: [
+                {name: 'description', content: 'Evacode - интернет магазин корейской косметики.'}
+            ],
+            link: [
+                {rel: 'icon', type: 'image/png', href: '/images/evacode/favicon.ico'}
+            ],
+        },
     },
-  },
 
-  css: ["@/assets/scss/app.scss"],
-  ssr:true,
-  modules: [
-    'nuxt-icon',
-    '@nuxt/image-edge',
-    // "@nuxtjs/i18n",
-    '@vueuse/nuxt',
-    // 'vue-toastification/nuxt',
-    [
-      "@pinia/nuxt",
-      {
-        autoImports: ["defineStore", "acceptHMRUpdate"],
-      },
+    css: ['@/assets/scss/app.scss'],
+    ssr: true,
+    modules: [
+        'nuxt-icon',
+        '@nuxtjs/sitemap',
+        '@nuxt/image-edge',
+        // "@nuxtjs/i18n",
+        '@vueuse/nuxt',
+        // 'vue-toastification/nuxt',
+        [
+            '@pinia/nuxt',
+            {
+                autoImports: ['defineStore', 'acceptHMRUpdate'],
+            },
+        ],
     ],
-
-
-  ],
-  pluginsIgnore: [
-    /^ignored-plugin/,
-    /another-ignored-plugin/,
-  ],
-  plugins: [
-
-    // {src: '~/node_modules/bootstrap/dist/js/bootstrap.js', mode: 'client'}
-    { src: './plugins/useBootstrap.client.ts', mode: 'client' },
-    { src: './plugins/showToast.client.js', mode: 'client' },
-    // { src: './plugins/paypal.client.js' }
-    // { src: 'https://checkout.stripe.com/checkout.js'}
-  ]
-  //    build: { vendor: 'vue-paypal-checkout'},
+    pluginsIgnore: [
+        /^ignored-plugin/,
+        /another-ignored-plugin/,
+    ],
+    plugins: [
+        {src: './plugins/useBootstrap.client.ts', mode: 'client'},
+        {src: './plugins/showToast.client.js', mode: 'client'},
+    ]
 })
