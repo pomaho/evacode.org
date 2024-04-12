5723cb41f493718d6350e75244bb7b696044f040
Roman Son <son.romario@gmail.com>
2024-04-12 12:11:12 +0300

    Fix cart hover popup widget

--- components/widgets/header-widgets.vue
+++ components/widgets/header-widgets.vue
@@ -6,7 +6,7 @@
           <div>
             <img alt src="/images/icon/layout4/cart.png" class="img-fluid">
             <i class="ti-shopping-cart"></i>
-            <span class="cart_qty_cls">{{cart.length}}</span>
+            <span class="cart_qty_cls">{{ cart.length }}</span>
           </div>
           <ul class="show-div shopping-cart" v-if="!cart.length">
             <li>Ваша корзина пуста.</li>
@@ -15,14 +15,14 @@
             <li v-for="(item,index) in cart" :key="index">
               <div class="media">
                 <nuxt-link :to="{ path: '/product/sidebar/'+item.id}">
-                  <img alt class="mr-3" :src='getImgUrl(item.images[0].src)'>
+                  <img alt class="mr-3" :src='item.images[0].url'>
                 </nuxt-link>
                 <div class="media-body">
                   <nuxt-link :to="{ path: '/product/sidebar/'+item.id}">
-                    <h4>{{item.title}}</h4>
+                    <h4>{{ item.title }}</h4>
                   </nuxt-link>
                   <h4>
-                    <span>{{item.quantity}} x {{ curr.symbol }}{{ item.price || currency }}</span>
+                    <span>{{ item.quantity }} x {{ curr.symbol }}{{ item.price }}</span>
                   </h4>
                 </div>
               </div>
@@ -36,7 +36,7 @@
               <div class="total">
                 <h5>
                   Итого :
-                  <span>{{ curr.symbol }}{{ cartTotal || currency }}</span>
+                  <span>{{ curr.symbol }}{{ cartTotal }}</span>
                 </h5>
               </div>
             </li>
@@ -57,79 +57,35 @@
   </div>
 </template>
 <script>
-import { useProductStore } from '~/store/products'
+import {useProductStore} from '~/store/products'
 import {useCartStore} from '~/store/cart'
-import { mapState } from 'pinia'
+import {mapState} from 'pinia'
+
 export default {
-  data() {
-    return {
-      currencyChange: {},
-      search: false,
-      searchString: '',
-      lang:[
-        {
-          code:'en',
-          name:'English'
-        },
-        {
-          code:'fr',
-          name:'French'
-        }
-      ]
-    }
-  },
   computed: {
-    ...mapState(useProductStore,{
-      searchItems: 'searchProducts',
+    ...mapState(useCartStore, {
+      cartTotal: (store) => store.cartTotalAmount,
     }),
-    language(){
-      return this.lang.filter((lang) => lang.code != this.$i18n.locale)
-    },
-    ...mapState(useCartStore,{
-      // cart:(store)=> store.cartItems,
-      cartTotal:(store)=> store.cartTotalAmount,
-    }),
-    cart(){
+    cart() {
       return useCartStore().cartItems
     },
-    curr(){
+    curr() {
       return useProductStore().changeCurrency
     }
 
   },
-  watch:{
-    searchString(){
-      useProductStore().searchProduct(this.searchString)
-    },
-    cart(){
+  watch: {
+    cart() {
       useCartStore().cartItems
     },
   },
   methods: {
-    getImgUrl(path) {
-      return ('/images/' + path)
-    },
-    openSearch() {
-      this.search = true
-    },
-    closeSearch() {
-      this.search = false
-    },
-    searchProduct() {
-    },
     removeCartItem: function (product) {
       useCartStore().removeCartItem(product)
-      if(this.cart.length==0 && this.$route.name=== 'page-account-checkout'){
+      if (this.cart.length === 0 && this.$route.name === 'page-account-checkout') {
         this.$router.replace('/page/account/cart')
       }
     },
-    setlang(name){
-      this.$i18n.locale = name
-    },
-    updateCurrency: function (currency, currSymbol) {
-      this.currencyChange = { value: currency, symbol: currSymbol }
-      useProductStore().changeCurrency2(this.currencyChange)
-    }
   },
 }
 </script>
--- store/cart.js
+++ store/cart.js
@@ -1,76 +1,78 @@
 // import products from '../data/products_1.json'
 import products from '../data/products.json'
-import { useStorage } from '@vueuse/core'
+import {useStorage} from '@vueuse/core'
 
 import {defineStore} from 'pinia'
-export const useCartStore =defineStore({
+
+export const useCartStore = defineStore({
     id: 'cart-store',
-    state:()=>{
-        return{
+    state: () => {
+        return {
             products: products.data,
-            cart:[],
-            abc:{}
+            cart: [],
+            abc: {}
         }
     },
-    actions:{
+    actions: {
         addToCart(payload) {
-          console.log('jemin')
+            console.log('jemin')
             const product = this.products.find(item => item.id === payload.id)
             const cartItems = this.cart.find(item => item.id === payload.id)
             const qty = payload.quantity ? payload.quantity : 1
             if (cartItems) {
-              cartItems.quantity = qty
+                cartItems.quantity = qty
             } else {
-              this.cart.push({
-                ...product,
-                quantity: qty
-              })
+                this.cart.push({
+                    ...product,
+                    quantity: qty
+                })
             }
             product.stock--
-          },
-          updateCartQuantity( payload)  {
+        },
+        updateCartQuantity(payload) {
             function calculateStockCounts(product, quantity) {
-              const qty = product.quantity + quantity
-              const stock = product.stock
-              if (stock < qty) {
-                return false
-              }
-              return true
-            }
-            this.cart.find((items, index) => {
-              if (items.id === payload.product.id) {
-                const qty = this.cart[index].quantity + payload.qty
-                const stock = calculateStockCounts(this.cart[index], payload.qty)
-                if (qty !== 0 && stock) {
-                  this.cart[index].quantity = qty
-                } else {
-                  // state.cart.push({
-                  //   ...product,
-                  //   quantity: qty
-                  // })
+                const qty = product.quantity + quantity
+                const stock = product.stock
+                if (stock < qty) {
+                    return false
                 }
                 return true
-              }
+            }
+
+            this.cart.find((items, index) => {
+                if (items.id === payload.product.id) {
+                    const qty = this.cart[index].quantity + payload.qty
+                    const stock = calculateStockCounts(this.cart[index], payload.qty)
+                    if (qty !== 0 && stock) {
+                        this.cart[index].quantity = qty
+                    } else {
+                        // state.cart.push({
+                        //   ...product,
+                        //   quantity: qty
+                        // })
+                    }
+                    return true
+                }
             })
-          },
-          removeCartItem(payload) {
-            this.cart = this.cart.filter((item)=>item.id!=payload.id)
+        },
+        removeCartItem(payload) {
+            this.cart = this.cart.filter((item) => item.id != payload.id)
 
 
-          },
-          setInitialCart(payload){
+        },
+        setInitialCart(payload) {
             this.cart = payload
-          }
+        }
     },
-    getters:{
+    getters: {
         cartItems: (state) => {
             return state.cart
-          },
-          cartTotalAmount: (state) => {
+        },
+        cartTotalAmount: (state) => {
             return state.cart.reduce((total, product) => {
-              return total + (product.price * product.quantity)
+                return total + (product.price * product.quantity)
             }, 0)
-          }
+        }
     }
 })
 
