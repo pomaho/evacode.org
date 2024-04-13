a75be8864f20700c2abbdc43ef4be5dd2261e64b
Roman Son <son.romario@gmail.com>
2024-04-13 12:13:45 +0300

    Restore navbar.vue widgets components

---
+++ components/widgets/navbar.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <!-- Sample menu definition -->
+    <div class="main-navbar">
+      <div id="mainnav">
+        <div class="toggle-nav" :class="leftSidebarVal ? 'toggle-button' : ''" @click="openmobilenav = true">
+          <i class="fa fa-bars sidebar-bar"></i>
+        </div>
+        <ul class="nav-menu" :class="{ opennav: openmobilenav }">
+          <li class="back-btn">
+            <div class="mobile-back text-end">
+              <span @click="openmobilenav = false">Back</span>
+              <i class="fa fa-angle-right ps-2" aria-hidden="true"></i>
+            </div>
+          </li>
+          <li v-for="(menuItem, index) in menulist" :key="index" :class="menuItem.megamenu ? 'mega-menu' : 'dropdown'">
+            <nuxt-link :to="menuItem.path"> {{menuItem.title}} </nuxt-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { mapState } from 'pinia'
+import { useMenuStore } from '~/store/menu'
+export default {
+  props: ['leftSidebarVal'],
+  data() {
+    return {
+      openmobilenav: false,
+    }
+  },
+  computed: {
+    ...mapState(useMenuStore, {
+      menulist: 'data'
+    })
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.toggle-nav {
+  &.toggle-button {
+    z-index: 1;
+  }
+}
+.router-link-exact-active {
+  color: var(--theme-deafult);
+}
+</style>
