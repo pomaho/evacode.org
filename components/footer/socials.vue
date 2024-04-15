faa518ab7f5595a9480867b2fd424f6b6816f850
Roman Son <son.romario@gmail.com>
2024-04-15 23:57:44 +0300

    Add social links

--- components/footer/socials.vue
+++ components/footer/socials.vue
@@ -5,9 +5,9 @@
 <template>
   <div class="footer-social">
     <ul>
-      <li><a href="#"><i class="fa fa-facebook-f"></i></a></li>
-      <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-      <li><a href="#"><i class="fa fa-telegram"></i></a></li>
+      <li><a target="_blank" href="https://www.facebook.com/groups/379782879558998/"><i class="fa fa-facebook-f"></i></a></li>
+      <li><a target="_blank" href="https://www.instagram.com/evacodeorg"><i class="fa fa-instagram"></i></a></li>
+      <li><a target="_blank" href="https://t.me/EvaCodeKR"><i class="fa fa-telegram"></i></a></li>
     </ul>
   </div>
 </template>
