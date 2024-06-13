b343629c1eb9266ea63035ff8cbfad358e714540
Roman Son <son.romario@gmail.com>
2024-06-13 19:22:22 +0300

    Contact page

--- pages/page/account/contact.vue
+++ pages/page/account/contact.vue
@@ -1,77 +1,67 @@
 <template>
-  <Header/>
-  <div>
-    <section class="contact-page section-b-space">
-      <div class="container">
-        <div class="row section-b-space">
-          <div class="col-lg-7 map">
-            <iframe
-                src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=ru&amp;q=Gyeonggi-do,%20Ansan-si,%20Danwon-gu,%20Choji-dong,%20742-3,%20210%ED%98%B8+(Evacode)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
-                allowfullscreen></iframe>
-          </div>
-          <div class="col-lg-5">
-            <div v-if="contacts" class="contact-right">
-              <ul>
-                <li>
-                  <div class="contact-icon">
-                    <img :src="phoneimage" alt="Generic placeholder image"/>
-                    <h6>Звоните</h6>
-                  </div>
-                  <div class="media-body">
-                    <p><phone-link :phone="contacts.phone" /> (там WhatsApp)</p>
-                  </div>
-                </li>
-                <li>
-                  <div class="contact-icon">
-                    <i class="fa fa-map-marker" aria-hidden="true"></i>
-                    <h6>Адрес</h6>
-                  </div>
-                  <div class="media-body">
-                    <p v-html="contacts.address"></p>
-                  </div>
-                </li>
-                <li>
-                  <div class="contact-icon">
-                    <img :src="emailimage" alt="Generic placeholder image"/>
-                    <h6>Email</h6>
-                  </div>
-                  <div class="media-body">
-                    <p><mail-link :email="contacts.email"/></p>
-                    <p></p>
-                  </div>
-                </li>
-              </ul>
+    <Header/>
+    <div>
+        <section class="contact-page section-b-space">
+            <div class="container">
+                <img class="contact-logo" src="/images/new_evacode/evacode-contact-logo.png"  alt=""/>
+                <h2 class="contact-title">Контакты</h2>
+                <div class="row contacts-row section-b-space">
+                    <div class="col-xl-7 col-12">
+                        <div v-if="contacts" class="contact-right">
+                            <h3 class="our-address">Наш адрес</h3>
+                            <p v-html="contacts.address"></p>
+                            <h5 class="work-hours">Режим работы:</h5>
+                            <p>
+                                Воскресенье - Пятница с 10:00 - 19:00<br>
+                                Суббота - Выходной день
+                            </p>
+
+                            <h3 class="our-address social">Соц сети</h3>
+
+                            <a target="_blank" :href="contacts.instagram" class="social-contact"><img src="/images/new_evacode/instagram.png" />Instagram</a>
+                            <a target="_blank" :href="contacts.telegram" class="social-contact"><img src="/images/new_evacode/telegram.png" />Telegram</a>
+                            <a target="_blank" :href="contacts.tiktok" class="social-contact"><img src="/images/new_evacode/tiktok.png" />Tiktok</a>
+                            <a target="_blank" :href="contacts.facebook" class="social-contact"><img src="/images/new_evacode/facebook.png" />Facebook</a>
+                        </div>
+                    </div>
+                    <div class="col-xl-5 col-12 map">
+                        <iframe
+                            src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=ru&amp;q=Gyeonggi-do,%20Ansan-si,%20Danwon-gu,%20Choji-dong,%20742-3,%20210%ED%98%B8+(Evacode)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+                            allowfullscreen></iframe>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  </div>
-  <Footer/>
+        </section>
+    </div>
+    <ShopBeautyTestimonials />
+    <ShopBeautyAboutSlider />
+    <Footer/>
 </template>
 <script setup>
-  import {useContactsStore} from '~~/store/contacts'
-  const contacts = await useContactsStore().contacts;
+import {useContactsStore} from '~~/store/contacts'
 
-  const phoneimage = '/images/evacode/icon/phone.png';
-  const emailimage = '/images/evacode/icon/email.png';
+const contacts = await useContactsStore().contacts;
 
-  useHead({
+const phoneimage = '/images/evacode/icon/phone.png';
+const emailimage = '/images/evacode/icon/email.png';
+
+useHead({
     titleTemplate: `%s - Контакты`,
     meta: [
-      {
-        name: 'description',
-        content: 'Evacode - интернет магазин корейской косметики - Контакты'
-      },
+        {
+            name: 'description',
+            content: 'Evacode - интернет магазин корейской косметики - Контакты'
+        },
     ]
-  });
+});
 </script>
 
 <style scoped>
 .map {
-  height: 435px;
+    height: 435px;
 }
+
 .contact-page label {
-  text-transform: none;
+    text-transform: none;
 }
 </style>
