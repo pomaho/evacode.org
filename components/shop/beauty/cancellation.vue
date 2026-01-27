6e499e10136303e4f47a3516838ffd7b6dfb16c3
Roman Son <son.romario@gmail.com>
2026-01-27 12:43:59 +0300

    Try Fix Store

--- components/shop/beauty/cancellation.vue
+++ components/shop/beauty/cancellation.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
-        <section v-if="deliveries.length" class="section-cancellation section-b-space">
+        <section v-if="deliveriesSafe.length" class="section-cancellation section-b-space">
             <div class="container">
                 <img class="cancellation-image" src="/images/new_evacode/cancellation.png" />
                 <h2 class="cancellation-title">Возврат и отмена</h2>
                 <div class="container-wrapper">
                     <div class="row">
-                        <div class="col-xl-6 col-12 text-column" v-html="deliveries[deliveries.length - 1].delivery_description">
+                        <div class="col-xl-6 col-12 text-column" v-html="deliveriesSafe[deliveriesSafe.length - 1].delivery_description">
                         </div>
                     </div>
                 </div>
@@ -16,9 +16,18 @@
 </template>
 
 <script setup>
-import {useDeliveryStore} from '~/store/delivery';
+const deliveries = ref([]);
+const deliveriesSafe = computed(() => Array.isArray(deliveries.value) ? deliveries.value : []);
 
-const deliveries = await useDeliveryStore().deliveries;
+onMounted(async () => {
+    try {
+        const res = await $fetch(`${useRuntimeConfig().public.apiBase}/core/delivery`);
+        deliveries.value = res?.results || [];
+    } catch (error) {
+        console.error(error);
+        deliveries.value = [];
+    }
+});
 useHead({
     titleTemplate: `%s - Доставка`,
     meta: [
